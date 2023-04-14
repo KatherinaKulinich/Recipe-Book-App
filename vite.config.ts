62d997d822d1acdfff8a705f238b7cfb79231c7e
KatherinaKulinich <kulinichcatherina@gmail.com>
2023-04-14 20:58:56 +0300

    json

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Recipe-Book-App/',
+  base: '/',
   plugins: [react()],
   build: { chunkSizeWarningLimit: 1600, },
 })
